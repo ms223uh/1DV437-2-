775d931f786cae225651148cfcbd32a238e7a4cf
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2015-11-30 15:43:15 +0100

    CameraClass - Done!

--- Assignment 2/Explosion/Game1/Game1/View/Camera.cs
+++ Assignment 2/Explosion/Game1/Game1/View/Camera.cs
@@ -8,7 +8,35 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Game1.View
 {
-    class Camera
+    namespace Game1.View
     {
+        class Camera
+        {
+            private float scaleY;
+            private float scaleX;
+
+            public Camera(Viewport viewport)
+            {
+                scaleX = viewport.Width / 2;
+                scaleY = viewport.Height / 2;
+            }
+
+            public float getScale(float radius, int boundWidth)
+            {
+                return (radius * 2.0f) * (float)scaleX / (float)boundWidth;
+            }
+
+            public Vector2 getScreenCord(Vector2 screenPosition)
+            {
+                int screenX = (int)((screenPosition.X * scaleX));
+                int screenY = (int)((screenPosition.Y * scaleY));
+                return new Vector2(screenX, screenY);
+            }
+
+            public Rectangle getRectangle(float x, float y)
+            {
+                return new Rectangle((int)(scaleX * x), (int)(scaleY * y), 150, 150);
+            }
+        }
     }
 }
